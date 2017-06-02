9bfde9f1fd7b8549d56ac76be6739ac910d0a325
Henrik Peters <inf101512@rz5-pc03.fh-wedel.de>
2017-06-02 09:14:23 +0200

    added more tests

--- bufferinterface/bufferinterface_test.go
+++ bufferinterface/bufferinterface_test.go
@@ -97,12 +97,138 @@ func TestRequest(t *testing.T) {
 }
 
 func TestFix(t *testing.T) {
+	ResetCounters()
+	
+	//Fill the puffer with pages
+	for i := 0; i < PufferSize; i++ {
+		if _, err := Request(i); err != nil {
+			t.Error(err)
+		}
+	}
+	
+	//Fix some pages
+	Fix(5)
+	Fix(7)
+	
+	if !puffer[pageMap[5]].isFixed {
+		t.Error("Page 5 is not fixed")
+	}
+	
+	if !puffer[pageMap[7]].isFixed {
+		t.Error("Page 7 is not fixed")
+	}
+	
+	//Fill the puffer with new pages
+	for i := PufferSize; i < PufferSize * 2; i++ {
+		if _, err := Request(i); err != nil {
+			t.Error(err)
+		}
+	}
+	
+	//Test if the fixed pages are still puffered
+	if !pageAvailInPuffer(5) {
+		t.Error("Page 5 is fixed but not in puffer")
+	}
+	
+	if !pageAvailInPuffer(7) {
+		t.Error("Page 5 is fixed but not in puffer")
+	}
+	
+	if !puffer[pageMap[5]].isFixed {
+		t.Error("Page 5 is not fixed")
+	}
+	
+	if !puffer[pageMap[7]].isFixed {
+		t.Error("Page 7 is not fixed")
+	}
+	
+	WriteCacheStats("TestFix")
 }
 
 func TestUnfix(t *testing.T) {
+	ResetCounters()
+	
+	if page, err := Request(1); err != nil {
+		t.Error(err)
+	} else {
+
+		if page == nil {
+			t.Error("Page 1 is nil")
+		}
+	}
+	
+	Fix(1)
+	
+	if !puffer[pageMap[1]].isFixed {
+		t.Error("Page 1 is not fixed")
+	}
+	
+	UnFix(1)
+	
+	if puffer[pageMap[1]].isFixed {
+		t.Error("Page 1 should be unfixed")
+	}
+	
+	WriteCacheStats("TestUnfix")
 }
 
 func TestUpdate(t *testing.T) {
+	ResetCounters()
+	
+	//Fill the puffer with pages
+	for i := 0; i < PufferSize; i++ {
+		if _, err := Request(i); err != nil {
+			t.Error(err)
+		}
+	}
+	
+	//Request page 8 and write some data
+	if page, err := Request(8); err != nil {
+		t.Error(err)
+	} else {
+
+		if page == nil {
+			t.Error("Page 8 is nil")
+		}
+
+		page[0] = 0x12
+		page[1] = 0x34
+		page[2] = 0x56
+		page[3] = 0xA8
+		page[4] = 0x9E
+		
+		//Mark this page as updated
+		Update(8)
+		
+		//Check the update-flag
+		if !puffer[pageMap[8]].isUpdated {
+			t.Error("Page 8 should have the update-flag set")
+		}
+	}
+	
+	//Fill the puffer with new pages
+	//Page 8 should be written to disk on a cache displacement
+	for i := PufferSize; i < PufferSize * 2; i++ {
+		if _, err := Request(i); err != nil {
+			t.Error(err)
+		}
+	}
+	
+	//Request page 8 and see if we get the correct data
+	if page, err := Request(8); err != nil {
+		t.Error(err)
+	} else {
+
+		if page == nil {
+			t.Error("Page 8 is nil")
+		}
+
+		if page[0] != 0x12 || page[1] != 0x34 || page[2] != 0x56 || page[3] != 0xA8 || page[4] != 0x9E {
+			t.Error("Page 8 does not contain saved data")
+		}
+	}
+	
+	WriteCacheStats("TestUpdate")
 }
 
 func TestWrite(t *testing.T) {
